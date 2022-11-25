8cd81c565b3101746ae78cd20af4e060ff668ec4
Henri Gros <henrigros1@gmail.com>
2022-11-25 11:03:29 +0100

    fix creation tables

--- data/v1_createDB.sql
+++ data/v1_createDB.sql
@@ -17,7 +17,7 @@ CREATE TABLE LesEquipes
 (
   numPA NUMBER(4),
   CONSTRAINT EQ_PK PRIMARY KEY (numPA),
-  CONSTRAINT EQ_FK_PA FOREIGN KEY numPA REFERENCES LesParticipants(numPA)
+  CONSTRAINT EQ_FK_PA FOREIGN KEY (numPA) REFERENCES LesParticipants(numPA)
 );
 
 CREATE TABLE LesSportifsEQ
@@ -30,9 +30,9 @@ CREATE TABLE LesSportifsEQ
   dateNaisSp DATE,
   numEq NUMBER(4),
   CONSTRAINT SP_PK PRIMARY KEY (numPA),
-  CONSTRAINT SP_FK_EQ FOREIGN KEY numEq REFERENCES LesEquipes(numPA),
-  CONSTRAINT SP_FK_PA FOREIGN KEY numPA REFERENCES LesParticipants(numPA),
-  CONSTRAINT SP_CK1 CHECK(numSp > 0),
+  CONSTRAINT SP_FK_EQ FOREIGN KEY (numEq) REFERENCES LesEquipes(numPA),
+  CONSTRAINT SP_FK_PA FOREIGN KEY (numPA) REFERENCES LesParticipants(numPA),
+  CONSTRAINT SP_CK1 CHECK(numPA > 0),
   CONSTRAINT SP_CK2 CHECK(categorieSp IN ('feminin','masculin')),
   CONSTRAINT SP_CK3 CHECK(numEq > 0)
 );
@@ -48,7 +48,7 @@ CREATE TABLE LesEpreuves
   nbSportifsEp NUMBER(4),
   dateEp DATE,
   CONSTRAINT EP_PK PRIMARY KEY (numEp),
-  CONSTRAINT EP_FK_DI FOREIGN KEY numDI REFERENCES LesDisciplines(numDI),
+  CONSTRAINT EP_FK_DI FOREIGN KEY (numDI) REFERENCES LesDisciplines(numDI),
   CONSTRAINT EP_CK1 CHECK (formeEp IN ('individuelle','par equipe','par couple')),
   CONSTRAINT EP_CK2 CHECK (categorieEp IN ('feminin','masculin','mixte')),
   CONSTRAINT EP_CK3 CHECK (numEp > 0),
@@ -61,8 +61,8 @@ CREATE TABLE Participe
   numEp NUMBER(4),
   medaille VARCHAR2(25),
   CONSTRAINT DI_PK PRIMARY KEY (numPA, numEp),
-  CONSTRAINT PAR_FK_PA FOREIGN KEY numPA REFERENCES LesParticipants(numPA),
-  CONSTRAINT PAR_FK_EP FOREIGN KEY numEp REFERENCES LesEpreuves(numEp),
+  CONSTRAINT PAR_FK_PA FOREIGN KEY (numPA) REFERENCES LesParticipants(numPA),
+  CONSTRAINT PAR_FK_EP FOREIGN KEY (numEp) REFERENCES LesEpreuves(numEp),
   CONSTRAINT PAR_CK2 CHECK(medaille IN ('or','argent','bronze'))
 
 );
@@ -71,3 +71,4 @@ CREATE TABLE Participe
 -- TODO 1.4a : ajouter la définition de la vue LesAgesSportifs
 -- TODO 1.5a : ajouter la définition de la vue LesNbsEquipiers
 -- TODO 3.3 : ajouter les éléments nécessaires pour créer le trigger (attention, syntaxe SQLite différent qu'Oracle)
+
