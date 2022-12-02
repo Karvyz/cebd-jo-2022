d4aa0caead05ad36b0bebcc7802fc707e6b7baf1
Henri Gros <henrigros1@gmail.com>
2022-12-02 09:08:11 +0100

    fct comp part 1

--- actions/v0_action_fct_comp_1_partie_1.py
+++ actions/v0_action_fct_comp_1_partie_1.py
@@ -20,7 +20,7 @@ class AppFctComp1Partie1(QDialog):
         # TODO 1.1 : fonction à modifier pour remplacer la zone de saisie par une liste de valeurs prédéfinies
         #  dans l'interface une fois le fichier ui correspondant mis à jour
         display.refreshLabel(self.ui.label_fct_comp_1, "")
-        if not self.ui.lineEdit_fct_comp_1.text().strip():
+        if not self.ui.comboBox.currentText().strip():
             self.ui.table_fct_comp_1.setRowCount(0)
             display.refreshLabel(self.ui.label_fct_comp_1, "Veuillez indiquer un nom de catégorie")
         else:
@@ -28,7 +28,7 @@ class AppFctComp1Partie1(QDialog):
                 cursor = self.data.cursor()
                 result = cursor.execute(
                     "SELECT numEp, nomEp, formeEp, nomDi, categorieEp, nbSportifsEp, strftime('%Y-%m-%d',dateEp,'unixepoch') FROM V0_LesEpreuves WHERE categorieEp = ?",
-                    [self.ui.lineEdit_fct_comp_1.text().strip()])
+                    [self.ui.comboBox.currentText().strip()])
             except Exception as e:
                 self.ui.table_fct_comp_1.setRowCount(0)
                 display.refreshLabel(self.ui.label_fct_comp_1, "Impossible d'afficher les résultats : " + repr(e))
